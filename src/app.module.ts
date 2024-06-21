b183f7e19f13ee7733059757fcc2ed79205bb598
Ryo Tanaka <ryochi.tanaka@gorillarush.com>
2024-06-21 12:09:48 +1000

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { GameController } from './game/game.controller';
+import { GameService } from './game/game.service';
+
+@Module({
+  imports: [],
+  controllers: [GameController],
+  providers: [GameService],
+})
+export class AppModule {}
